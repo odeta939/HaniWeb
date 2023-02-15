cf4805cea67dea21dcf72904b6267373ca26152b
odeta939 <odetapenikaite@gmail.com>
2023-02-15 14:26:45 +0100

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	printWidth: 120,
+	semi: true,
+	arrowParens: "always",
+	singleQuote: false,
+	trailingComma: "es5",
+	bracketSpacing: true,
+	useTabs: true,
+	tabWidth: 4,
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+};
